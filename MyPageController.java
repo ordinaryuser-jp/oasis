bad35fb92dc1abd5efbb04c10c92980589b8f0e8
ordinaryuser-jp <ordinaryuser.jp@gmail.com>
2023-10-26 17:38:22 +0900

    oasis

---
+++ MyPageController.java
@@ -0,0 +1,45 @@
+package jp.co.internous.oasis.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import jp.co.internous.oasis.model.domain.MstUser;
+import jp.co.internous.oasis.model.mapper.MstUserMapper;
+import jp.co.internous.oasis.model.session.LoginSession;
+
+/**
+ * マイページに関する処理を行うコントローラー
+ * @author インターノウス
+ *
+ */
+@Controller
+@RequestMapping("/oasis/mypage")
+public class MyPageController {
+	
+	/*
+	 * フィールド定義
+	 */
+	@Autowired
+	private MstUserMapper userMapper;
+	
+	@Autowired
+	private LoginSession loginSession;
+	
+	/**
+	 * マイページ画面を初期表示する。
+	 * @param m 画面表示用オブジェクト
+	 * @return マイページ画面
+	 */
+	@RequestMapping("/")
+	public String index(Model m) {
+		MstUser user = userMapper.findByUserNameAndPassword(loginSession.getUserName(),loginSession.getPassword());
+		
+		m.addAttribute("user",user);
+		m.addAttribute("loginSession",loginSession);
+	
+		return "my_page";
+		
+	}
+}
